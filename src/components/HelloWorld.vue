a3188b4d628c6c745aa7d9ad61f6149cfc59c655
rocioaltairr <42332771+rocioaltairr@users.noreply.github.com>
2020-02-14 11:58:48 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,106 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="left">
+      <h1>{{ title }}</h1>
+      <form @submit.prevent="addLink">
+        <input class="link-input" type="text" placeholder="Add a Link" v-model="newLink" />
+      </form>
+      <ul>
+        <li v-for="(link, index) in links" v-bind:key="index">
+          {{ link }}
+          <button v-on:click="removeLinks(index)" class="rm">Remove</button>
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <stats />
+    </div>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import stats from '@/components/stats.vue'
+  import {
+    mapState,
+    mapMutations,
+    mapActions
+  } from 'vuex'
 
+  export default {
+    name: 'HelloWorld',
+    data() { // Add this:
+      return {
+        newLink: ''
+      }
+    },
+    components: {
+      stats
+    },
+    computed: mapState([
+      'title',
+      'links'
+    ]),
+    methods: { // Add this:
+      ...mapMutations([
+        'ADD_LINK'
+      ]),
+    ...mapActions([                  // Add this
+      'removeLink'
+    ]),
+      addLink: function () {
+        this.ADD_LINK(this.newLink)
+        this.newLink = ''
+      },
+      removeLinks: function (link) { // Add this
+        this.removeLink(link)
+      }
+    }
+  }
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .hello {
+    display: flex;
+  }
+
+  .right {
+    width: 50%;
+    background: #bcdac8;
+  }
+
+  .left {
+    background: #d9f7e5;
+    width: 50%;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  li {
+    background: aliceblue;
+    width: 50%;
+    padding: 10px;
+    margin: 5px;
+  }
+  
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    title: 'My Custom Title',
+    links: [
+      'http://google.com',
+      'http://coursetro.com',
+      'http://youtube.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {        // Add this:
+      state.links.splice(link, 1)
+    }
   },
   actions: {
+    removeLink: (context, link) => {       // Add this:
+      context.commit("REMOVE_LINK", link)
+    }
   },
   modules: {
   }
